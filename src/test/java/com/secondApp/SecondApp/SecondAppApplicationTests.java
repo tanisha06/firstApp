48bf677651a417c52e6f2ba15520229d1ee8bdef
tanishaseth <sethtanisha9@gmail.com>
2025-12-11 13:54:51 +0530

    Add Spring Boot project with Excel reading POST endpoint

---
+++ src/test/java/com/secondApp/SecondApp/SecondAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondApp.SecondApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
